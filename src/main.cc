23c7b7a42826435864212430663b2bc5687d8e4a
Almagest <almagest0328@gmail.com>
2021-07-01 18:43:33 +0900

    Add TODO(#1)

--- src/main.cc
+++ src/main.cc
@@ -5,38 +5,13 @@
 #include "SatSize.hh"
 #include "Terminal.hh"
 
-// Map Encoding lists
-// # : Wall
-// P : Start position of the player
-// O : Rocks to move
-// G : Goal Place
-// . : Blank Position
-constexpr const char* simpleMap =
-    "#####\n"
-    "#POG#\n"
-    "#####\n";
-
-constexpr const char* map1 =
-    ".....................................\n"
-    ".#############...........#####.......\n"
-    ".#........####...........#...#.......\n"
-    ".#........################...#.......\n"
-    ".#.....G..#..........O.......#.......\n"
-    ".#....G.G.#..........O.......#.......\n"
-    ".#.....G..#..........O...P...#.......\n"
-    ".#...................O.......#.......\n"
-    ".#...........................#.......\n"
-    ".#############################.......\n"
-    ".....................................\n"
-    ".....................................\n";
-
-const char* maps[2] = {simpleMap, map1};
+#include "map.hh"
 
 int main()
 {
 
-    sokoban::Game game(static_cast<const char**>(maps), 2);
-
+    // TODO(#1): remove the second parameter if it is possible
+    sokoban::Game game(static_cast<const char**>(maps), 3);
     game.run();
 
     return 0;
